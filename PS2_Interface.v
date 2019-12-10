a835749804904377a62c1778d45918867d4952ac
woooks <xrjftd@hotmail.com>
2019-12-09 22:09:37 -0500

    Add files via upload

--- PS2_Interface.v
+++ PS2_Interface.v
@@ -1,4 +1,4 @@
-module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, last_data_received);
+module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, last_data_received, up_kb, down_kb, left_kb, right_kb, space_kb);
 
 	input 			inclock, resetn;
 	inout 			ps2_clock, ps2_data;
@@ -8,13 +8,48 @@ module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key
 
 	// Internal Registers
 	reg			[7:0]	last_data_received;	
+	output reg up_kb, down_kb, left_kb, right_kb, space_kb;
+	reg [31:0] count;
+	
+	initial begin
+		count <= 0;
+	end
 	
 	always @(posedge inclock)
 	begin
 		if (resetn == 1'b0)
 			last_data_received <= 8'h00;
-		else if (ps2_key_pressed == 1'b1)
+		else if (ps2_key_pressed == 1'b1) begin
 			last_data_received <= ps2_key_data;
+			
+			if (ps2_key_data == 8'h75) begin
+				up_kb <= 0;
+			end
+			else if (ps2_key_data == 8'h72) begin
+				down_kb <= 0;
+			end
+			else if (ps2_key_data == 8'h74) begin
+				right_kb <= 0;
+			end
+			else if (ps2_key_data == 8'h6B) begin
+				left_kb <= 0;
+			end
+			else if (ps2_key_data == 8'h29) begin
+				space_kb <= 0;
+			end
+			
+		end
+		
+		count <= count + 1;
+		if (count >= 32'd9500000) begin
+			count <= 0;
+			up_kb <= 1;
+			down_kb <= 1;
+			left_kb <= 1;
+			right_kb <= 1;
+			space_kb <= 1;
+		end
+		
 	end
 	
 	PS2_Controller PS2 (.CLOCK_50 			(inclock),
